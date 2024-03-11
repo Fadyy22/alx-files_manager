3422bdc51de0d30e481b97cc4d6d3a16551380cf
Fadyy22 <fadyalaa441@gmail.com>
2024-03-11 13:06:24 +0200

    post new user endpoint

---
+++ controllers/UsersController.js
@@ -0,0 +1,24 @@
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+
+export default class UserController {
+  static postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+    }
+    user = dbClient.db().collecion('users').findOne({ email: email });
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+    }
+    newUser = dbClient.db().collecion('users').insertOne({
+      email: email,
+      password: sha1(password)
+    });
+    return res.status(201).json({ email: newUser.email, id: newUser._id });
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
 
 const mountRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+  app.post('/users', UserController.postNew);
 };
 
 export default mountRoutes;
