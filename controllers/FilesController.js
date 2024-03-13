c7a13706900814fe2ee667ba945fc2e4bcc8284e
Fadyy22 <fadyalaa441@gmail.com>
2024-03-13 21:45:41 +0200

    create folders

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -22,18 +22,22 @@ export default class FilesController {
       const file = await dbClient.client.db('files_manager')
         .collection('files')
         .findOne({
-          _id: ObjectId(parentId)
+          _id: ObjectId(parentId),
         });
       if (!file) {
         return res.status(400).json({ error: 'Parent not found' });
       }
-      if (file && file.type !== 'folder') {
+      if (file.type !== 'folder') {
         return res.status(400).json({ error: 'Parent is not a folder' });
       }
     }
     const path = process.env.FOLDER_PATH || '/tmp/files_manager/';
     const fileName = v4();
-    fs.writeFileSync(path + fileName, Buffer.from(data, 'base64').toString());
+    if (type === 'folder') {
+      fs.mkdirSync(path + fileName);
+    } else {
+      fs.writeFileSync(path + fileName, Buffer.from(data, 'base64').toString());
+    }
     const file = await dbClient.client.db('files_manager').collection('files').insertOne({
       userId: req.user._id,
       name,
